2612094dca757bbb83c1c9c258b10b025f9488da
elliotyou <m9402311@mail.ntust.edu.tw>
2021-04-27 19:29:05 +0800

    add mongoose

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
