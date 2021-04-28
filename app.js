d4955794a72ff70a592bcac97447401dbdd05138
elliotyou <m9402311@mail.ntust.edu.tw>
2021-04-28 09:29:15 +0800

    add create function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 2-3-A3
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
 const app = express()
@@ -21,6 +22,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   Restaurant.find()
@@ -33,6 +35,27 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   return Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
+})
+
+app.get('/restaurant/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+
+  return Restaurant.create({
+    name: req.body.name,
+    name_en: req.body.name_en,
+    category: req.body.category,
+    image: req.body.image,
+    location: req.body.location,
+    phone: req.body.phone,
+    google_map: req.body.google_map,
+    rating: req.body.rating,
+    description: req.body.description,
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // app.get('/restaurants/:restaurant_id', (req, res) => {
