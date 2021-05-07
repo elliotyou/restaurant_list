759d2e4bb6f3c4e6a4c797555d5dcdc060d55fc1
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-07 08:55:18 +0800

    modify searchsort

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const searchsort = require('./modules/searchsort')
 
-router.use(home)
+router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/searchsort', searchsort)
 
--- routes/modules/searchsort.js
+++ routes/modules/searchsort.js
@@ -3,47 +3,60 @@ const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
 
-router.get('/', (req, res) => {
-  const sort = req.query.sort
-  const keyword = req.query.keyword.trim()
-  const searchCondition = {
+function generateSearchCondition(keyword) {
+  return {
     "$or": [
       { "name": { $regex: `${keyword}`, $options: '$i' } },
       { "category": { $regex: `${keyword}`, $options: '$i' } }
     ]
   }
+}
 
-  let sortMapping
-  let sortForReturn = {
-    isNameAsc: false,
-    isNameDesc: false,
-    isCategoryAsc: false,
-    isRatingDesc: false
+function generateObjectForSort(sortString) {
+  let outputObject = {
+    sortForExpress: {},
+    sortForHandlebars: {
+      isNameAsc: false,
+      isNameDesc: false,
+      isCategoryAsc: false,
+      isRatingDesc: false
+    }
   }
 
-  switch (sort) {
+  switch (sortString) {
     case 'isNameAsc':
-      sortMapping = { name: 'asc' }
-      sortForReturn.isNameAsc = true
+      outputObject.sortForExpress = { name: 'asc' }
+      outputObject.sortForHandlebars.isNameAsc = true
       break
     case 'isNameDesc':
-      sortMapping = { name: 'desc' }
-      sortForReturn.isNameDesc = true
+      outputObject.sortForExpress = { name: 'desc' }
+      outputObject.sortForHandlebars.isNameDesc = true
       break
     case 'isCategoryAsc':
-      sortMapping = { category: 'asc' }
-      sortForReturn.isCategoryAsc = true
+      outputObject.sortForExpress = { category: 'asc' }
+      outputObject.sortForHandlebars.isCategoryAsc = true
       break
     case 'isRatingDesc':
-      sortMapping = { rating: 'desc' }
-      sortForReturn.isRatingDesc = true
+      outputObject.sortForExpress = { isRatingDesc: 'desc' }
+      outputObject.sortForHandlebars.isRatingDesc = true
       break
   }
+
+  return outputObject
+}
+
+router.get('/', (req, res) => {
+  const sort = req.query.sort
+  const keyword = req.query.keyword.trim()
+  const searchCondition = generateSearchCondition(keyword)
+  const objectForSort = generateObjectForSort(sort)
+  const sortForExpress = objectForSort.sortForExpress
+  const sortForHandlebars = objectForSort.sortForHandlebars
 
   return Restaurant.find(searchCondition)
     .lean()
-    .sort(sortMapping)
-    .then(restaurants => res.render('index', { restaurants, keyword, sortForReturn }))
+    .sort(sortForExpress)
+    .then(restaurants => res.render('index', { restaurants, keyword, sortForHandlebars }))
     .catch(error => console.log(error))
 })
 
