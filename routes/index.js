acca407d7a2d48b9f89376329747feeb3bf94078
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-27 21:10:30 +0800

    add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const searchsort = require('./modules/searchsort')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/searchsort', searchsort)
+router.use('/users', users)
 
 module.exports = router
