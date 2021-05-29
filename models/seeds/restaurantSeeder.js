5692956103824669aa4032532b3810b988f1c72e
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-29 13:15:40 +0800

    modify restaurantSeeder to have user binding

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,53 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const Restaurant = require('../restaurant')
 const db = require('../../config/mongoose')
 
 const restaurants = require('./restaurant.json')
+const user = require('../user')
+
+const SEED_USER = {
+  name: 'zzz',
+  email: 'zzz@zzz.zzz',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  restaurants.results.forEach(restaurant => {
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => user.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Restaurant.create({ name: `name-${i}`, userId })
+      ))
     })
-  });
-  console.log('done.')
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
+
+  // restaurants.results.forEach(restaurant => {
+  //   Restaurant.create({
+  //     name: restaurant.name,
+  //     name_en: restaurant.name_en,
+  //     category: restaurant.category,
+  //     image: restaurant.image,
+  //     location: restaurant.location,
+  //     phone: restaurant.phone,
+  //     google_map: restaurant.google_map,
+  //     rating: restaurant.rating,
+  //     description: restaurant.description
+  //   })
+  // });
 })
 
